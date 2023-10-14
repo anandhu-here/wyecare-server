1cb14c293e3788c3b9ebdcd0ab1c56ece0f1cc5e
Anandhu Satheesh <anandhusatheesh@Anandhus-MacBook-Air.local>
2023-10-14 21:31:58 +0100

    code

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -162,7 +162,7 @@ module.exports = (app, db) => {
             const randomHash = getRandomHash(10);
             console.log(randomHash, email)
             const codeQuery = await db.query(
-                `update home_guest_user set code = 'anandhu' where username = ${email}`
+                `update home_guest_user set code = 'anandhu' where username = $1`, [email]
             )
             res.status(200).send({message:"Success"})
 
