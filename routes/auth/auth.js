e2c7f3dd8a4dd0f6d561a2c882a252daa95b4b0f
Anandhu Satheesh <anandhusatheesh@Anandhus-MacBook-Air.local>
2023-10-30 02:51:26 +0000

    build

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -597,7 +597,7 @@ module.exports = (app, db) => {
                 (carer_id, days, month, year) 
                 values ($1, $2, $3, $4) 
                 on conflict (carer_id, month, year)
-                do update set availability.days = availability.days || $2::jsonb[]
+                do update set days = availability.days || $2::jsonb[]
                 returning *;
             `, [carer_id, days, month, year])
 
