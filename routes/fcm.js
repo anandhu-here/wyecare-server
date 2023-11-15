b327c2967baae575eb688f3320811cffbe3cf3dc
Anandhu Satheesh <anandhusatheesh@Anandhus-MacBook-Air.local>
2023-11-15 01:06:52 +0000

    home remove

--- routes/fcm.js
+++ routes/fcm.js
@@ -2,10 +2,13 @@
 module.exports = (app, db) => {
     app.post('/fcm/update-token', async(req, res)=>{
         const { token, userId } = req.body;
+        console.log(token, userId, 'ff')
         try{
             const query = await db.query(`
-                update users set fcm_token = $1 where id = $2 returning *;
+                update users set fcm_token = ${token} where id = ${userId} returning *;
             `, [token. userId])
+
+            
             res.status(200).send(query.rows[0])
         }
         catch(error){
