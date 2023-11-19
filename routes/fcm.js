7fdc477f8d8e9ae01900aeca686e6d179afcc38c
Anandhu Satheesh <anandhusatheesh@Anandhus-MacBook-Air.local>
2023-11-19 01:54:54 +0000

    home remove

--- routes/fcm.js
+++ routes/fcm.js
@@ -17,6 +17,21 @@ module.exports = (app, db) => {
             res.status(400).send(error)
         }
     })
+    app.get('/fcm/get-notifications', async(req, res)=>{
+        const { reciever } = req.query;
+        try{
+            const query = await db.query(`
+                select * from notifications where reciever = $1
+            `, [reciever])
+
+            
+            res.status(200).send(query.rows[0])
+        }
+        catch(error){
+            console.log(error, 'error')
+            res.status(400).send(error)
+        }
+    })
 
     // app.post('/fcm/send-token', async(req, res)=>{
     //     const { userId} = req.body;
